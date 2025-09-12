7e5f070701469ee549c8297fc7775b2ae342ddf0
long. <dailong581@gmail.com>
2025-09-12 20:29:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Xin chào từ sâu gpt 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ streaky- đang chạy ở cổng ${PORT}`);
+});
